6749e4e11e921bd3dc478b88d760da91ae6ad683
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-10-15 19:55:10 +0200

    Assets general cleanup & naming change

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ export const Logo = () => {
     <div className="logo">
       <img
         className="logo__icon"
-        src="/assets/icon-wallet.png"
+        src="/assets/icon-wallet.svg"
         alt="wallet icon"
       />
       <h2 className="logo__title">Wallet</h2>
