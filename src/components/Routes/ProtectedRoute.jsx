b585a09aed9dd77f05da7b12b03ceef8cf0c27a2
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-10-23 20:25:53 +0200

    Add PropTypes & JSDoc to Routes

--- src/components/Routes/ProtectedRoute.jsx
+++ src/components/Routes/ProtectedRoute.jsx
@@ -1,9 +1,17 @@
+import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
-
+/**
+ * @param {{ component: React.ReactNode, redirectTo?: string }} props
+ */
 export const ProtectedRoute = ({ component: Component, redirectTo = "/" }) => {
   const { isAuth, isRefreshing } = useAuth();
   const shouldRedirect = !isAuth && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+};
+
+ProtectedRoute.propTypes = {
+  component: PropTypes.node.isRequired,
+  redirectTo: PropTypes.string,
 };
--- src/components/Routes/RestrictedRoute.jsx
+++ src/components/Routes/RestrictedRoute.jsx
@@ -1,8 +1,16 @@
+import PropTypes from "prop-types";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
-
+/**
+ * @param {{ component: React.ReactNode, redirectTo?: string }} props
+ */
 export const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => {
   const { isAuth } = useAuth();
 
   return isAuth ? <Navigate to={redirectTo} /> : Component;
+};
+
+RestrictedRoute.propTypes = {
+  component: PropTypes.node.isRequired,
+  redirectTo: PropTypes.string,
 };
